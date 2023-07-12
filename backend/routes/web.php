94635f95b7835bedf0cca1c7e1b686ac08b90bc1
mohamedderbali <mohamed.derbali@esprit.tn>
2023-07-12 13:04:18 +0100

    fix backend application

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $message = [
+        'message' => 'Welcome to the application!'
+    ];
+
+    return new Response($message, 200);
 });
